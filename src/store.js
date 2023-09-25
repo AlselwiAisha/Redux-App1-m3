954aa7c9105b49018f8a1e8eb6c8a35bae4d242c
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-09-25 20:57:08 +0300

    Add cart code

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
-import {configureStore} from "@reduxjs/toolkit";
-
-export const store=configureStore({
-    reducer:{},
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
+import modalReducer from './features/modal/modalSlice';
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    modal: modalReducer,
+  },
 });
